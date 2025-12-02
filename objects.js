96c9d23a33f3e70767421d1c8f002df3cbcd4533
Marcos Vidal <devtionit@gamil.com>
2025-12-01 21:24:09 -0300

    ADD objects examples

---
+++ objects.js
@@ -0,0 +1,19 @@
+
+const car = {
+    brand: "Ford",
+    model: "Fiesta",
+    year: 2020,
+    isAutomatic: true,
+    doors: 4,
+    start: () => console.log("El auto está arrancando") // Método
+}
+
+console.log(car)
+
+const brandCar = car.brand
+
+console.log(brandCar)
+
+car.start()
+
+console.log(console)
